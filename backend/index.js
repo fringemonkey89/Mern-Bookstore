13633a189e1f751ea5468976396dd1d93ddf78cc
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-20 10:05:51 -0500

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,29 @@
 const  express = require ('express')
+const mongoose = require('mongoose')
 const PORT = 5555
 const app = express();
 
+app.use(express.json())
 
-
-app.listen(PORT, () => {
-    console.log(`server is listening to port: ${PORT}`)
+mongoose.connect('mongodb://127.0.0.1:27017/books-collection', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`server is listening to port: ${PORT}`)
+    })
 })
+.catch((error) => {
+    console.log(error)
+})
+
+mongoose.set('debug', true )
+
+
+app.get('/', (req, res) => {
+    console.log(req)
+    return res.status(234).send('Welcome to Mern stack tutorial')
+})
+
+
+
