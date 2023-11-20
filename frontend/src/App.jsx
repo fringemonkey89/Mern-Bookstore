2e6205bf74bff626cf0f8a9260634ee09f78e884
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-20 13:03:47 -0500

    added component files and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditeBooks';
+import DeleteBook from './pages/DeleteBooks';
 
 const App = () => {
   return (
-    <div> App</div>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/books/create' element={<CreateBook />} />
+        <Route path='/books/details/:id' element={<ShowBook />} />
+        <Route path='/books/edit/:id' element={<EditBook />} />
+        <Route path='/books/delete/:id' element={<DeleteBook />} />
+    </Routes>
   )
 }
 
