e428d23c0259d4d8360328961bc98521e511ff00
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-21 10:19:11 -0500

    added components and imported packages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Spinner from '../components/Spinner.jsx';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
 const Home = () => {
+    const [books, setBooks] = useState([]);    
+    const [loading, setLoading] = useState(false);  
+    useEffect(() => {
+
+    }, [])
     return (
         <div> Home </div>
     )
