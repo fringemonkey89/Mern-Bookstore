48653c7a2d08941406aeb1eee3a019aaa4ec0128
fringemonkey89 <markomajeticjr@gmail.com>
2023-11-30 12:22:50 -0500

    adding initial files

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ const PORT = 5555
 const app = express();
 const booksRoute = require('./routes/booksRoute.js')
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+  
+
 app.use(express.json())
 app.use('/books', booksRoute)
 
